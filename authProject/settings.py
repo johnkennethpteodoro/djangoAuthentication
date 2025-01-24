a807b887cb70bc88f4c387cad77efc96aa47176b
chods <johnkennethpteodoro@gmail.com>
2025-01-24 20:17:46 +0800

    fix

--- authProject/settings.py
+++ authProject/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-or@4nnx)l^84z_7!%e)qvp7yg&&k0hh$bt6)586jv_k=e2x@aj'
 DEBUG = True
 ALLOWED_HOSTS = ['.vercel.app']
 
